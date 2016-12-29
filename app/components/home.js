72ab811e4a2793c5616ad16f9bd3de91bf7967ca
Timur Kiyivinski <deathseclipse@gmail.com>
2016-12-29 17:09:03 +0800

    [DONE] Cycle example

--- app/components/home.js
+++ app/components/home.js
@@ -1,11 +1,18 @@
-import xs from 'xstream'
-import { div, p } from '@cycle/dom'
+import { div, p, input } from '@cycle/dom'
 
 const HomeComponent = sources => {
-  const vdom$ = xs.of({ err: false, message: 'Home page' })
-    .map(data => div([
-      p(`${data.message}`)
-    ]))
+  // Get events from .input-name as stream
+  const name$ = sources.DOM.select('.input-name')
+    .events('input')
+    .map(event => event.target.value)
+    .startWith('Input something into the field below') // Begin with an initial value for the stream
+
+  const vdom$ = name$.map(data => div('.card .card-inverse .card-primary', [
+    div('.card-block', [
+      p('.card-text', data), // Assign value from input as text
+      input('.form-control .input-name') // Take note of the .input-name class
+    ])
+  ]))
 
   return {
     DOM: vdom$
